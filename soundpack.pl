77ee9f8b99227624d1cb06aa03f7d3973d52c339
Storm Dragon <stormdragon2976@gmail.com>
2012-11-18 02:22:12 -0500

    Fixed a typo that was causing a crash in sound.pl.

--- soundpack.pl
+++ soundpack.pl
@@ -3,28 +3,16 @@
 #Published under the Floodgap Free Software License: http://www.floodgap.com/software/ffsl/
 
 
-#What is it?
-#This extension notifies you of new tweets using sounds.
-#There are different sounds for different types of notifications, replies and dms sound different from standard tweets.
-
-#Usage:
-#This extension works automatically. Just call it when you run TTYtter or add it to your ext line in your ~/.ttytterrc
-#This extension uses 2 settings that can optionally be added to your ~/.ttytterrc
-#extpref_soundpack=soundpack_name
-#extpref_sound_command=sound-command for example, sox users would use:
-#extpref_sound_command=play -q
-#for more information on extensions visit http://www.floodgap.com/software/ttytter/adv.html
-
 sub notifier_soundpack
 {
         my $class = shift;
         my $text = shift;
         my $ref = shift;
-        #Use extpref_sound_command in .ttytterrc to change to appropriate sound system such as ogg123 -q or paplay for pulseaudio.
-the default is play -q which requires sox be installed.
+        #Use extpref_sound_command in .ttytterrc to change to appropriate sound system such as ogg123 -q or paplay.
+        #is play.
         if (!$extpref_sound_command)
         {
-            $soundSystem = "play -q";
+            $soundSystem = "play -q -V0";
         }
         else
         {
